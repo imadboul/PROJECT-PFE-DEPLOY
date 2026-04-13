bb269e14abc287ee3b5611b1284de19fc2d7b086
imadboul <didouredjas2020@gmail.com>
2026-04-13 18:07:33 +0100

    immh

--- backend/projectpfe/order_client/views.py
+++ backend/projectpfe/order_client/views.py
@@ -59,13 +59,18 @@ def order(request):
             )
 
     if request.method == 'GET':
+        
 
         paginator = MyPagination()
 
         if request.role == 'client':
             queryset = Orderclient.objects.filter(client_id=request.user_id)
         else:
-            queryset = Orderclient.objects.all()
+            client_id = request.data.get('client_id')
+            if client_id:
+                queryset = Orderclient.objects.filter(client_id = client_id)
+            else:
+                queryset = Orderclient.objects.all()
 
         result_page = paginator.paginate_queryset(queryset, request)
         orders = OrderreadSerializer(result_page, many=True)
@@ -78,7 +83,29 @@ def order(request):
     
 
         
+@api_view(['GET'])
+@jwt_must
+@role_required(['Admin', 'superAdmin'])
+def getclients(request):
 
+    paginator = MyPagination()
+
+    
+    queryset = Client.objects.filter(ordersclient__isnull=False).distinct()
+
+    result_page = paginator.paginate_queryset(queryset, request)
+
+    clients = ClientreadSerializer(result_page, many=True)
+
+    return success_response(
+        data=paginated_response(paginator, clients),
+        message="Clients retrieved successfully",
+        status_code=status.HTTP_200_OK
+    )
+    
+
+        
+    
         
 @api_view(['POST'])
 @jwt_must
