cbd7db57480710a97f11aaa4cb54177bbb1dd9cf
Wassim Lahlah <wassimlahlah06@gmail.com>
2026-04-19 17:37:51 +0200

    wassim

---
+++ frontend/src/context/services/invoiceService.jsx
@@ -0,0 +1,13 @@
+import api from "../../api/axios";
+
+export const getInvoices = () =>
+    api.get("/invoices/filter/");
+
+export const getInvoiceData = () =>
+    api.get("/invoices/invoicedata/");
+
+export const getInvoicePDF = (id) =>
+    api.get(`/invoices/invoicePDF/${id}/`, { responseType: "blob" });
+
+export const validateInvoices = (type, data) =>
+    api.put(`/invoices/validate/${type}/`, data);
